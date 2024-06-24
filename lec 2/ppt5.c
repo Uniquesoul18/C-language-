ac5315b9e1a194232b18ec4de6777a93b7fe1dc3
Uniquesoul18 <soumyatae1831@gmail.com>
2024-06-24 11:28:49 +0530

    Add files via upload

---
+++ lec 2/ppt5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	//datatype var_name;//syntax
+	int x;
+	x=50;
+	int y;
+	y=30;
+	 
+	printf("x+y=%d\n",x+y);
+	printf("x-y=%d\n",x-y);
+	printf("x*y=%d\n",x*y);
+	printf("x/y=%d\n",x/y);
+	printf("x%y=%d\n",x%y); 
+	
+}
---
+++ lec 2/ppt6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf(" ---------\n");
+	printf("|         |\n");
+	printf("| 7x7=49  |\n");
+	printf("|         |\n");
+	printf(" ---------\n");
+}
