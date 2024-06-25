8b0ca3a9024cebc8de4615bb8df7b9ad50a4c1a7
Uniquesoul18 <soumyatae1831@gmail.com>
2024-06-25 12:17:17 +0530

    Update pr2.c

--- PR.1/pr2.c
+++ PR.1/pr2.c
@@ -3,22 +3,22 @@
 
 void main()
 {
-	int salary,HRA,DA,TA,Grosssalary;
+	int salary,Grosssalary,a,b,c;
 	 
     printf("Enter salary:");
     scanf("%d",&salary);
-     int HRA=10%
-     printf("HRA=%d");
-     int DA=5%;
-     printf("DA=%d");
-     int TA=8%; 
-  printf("TA=%d");
+     int HRA=10;
+     
+     int DA=5;
+    
+     int TA=8;
+  
  a=salary*HRA/100;
-  Db=salary*DA/100;
+  b=salary*DA/100;
   c=salary*TA/100;
-  grosssalary=salary+a+b+c;
+  Grosssalary=salary+a+b+c;
 
-  printf("grosssalry:%d",grosssalary);
+  printf("Grosssalry:%d",Grosssalary);
     
 
 }
