b53eb243d47985efe2de56f5da3e8799d6ee24d7
Uniquesoul18 <soumyatae1831@gmail.com>
2024-07-01 22:33:31 +0530

    Add files via upload

---
+++ sequence/selec.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	int X=8;
+	int Y=3;
+
+	 
+	(X>Y)?printf("Y is minimum \n"):printf("X is minimum");
+	
+	
+
+	int Z=12;
+	
+	(Y<Z)?printf("Y is minimum"):printf("Z is minimum ");
+	
+}
---
+++ sequence/selec2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int X,Y,Z;
+	printf("Enter Number :");
+	scanf("%d",&X);
+	printf("Enter Number:");
+	scanf("%d",&Y);
+	(X>Y)?printf("Y is minimun\n"):printf("X is minimum\n");
+	
+	printf("Enter Number:");
+	scanf("%d,&Z");
+	printf("Enter Number:");
+	scanf("%d",&Y);
+	(Z>Y)?printf("Z is minimum"):printf ("Y is minimum");
+	
+
+}
