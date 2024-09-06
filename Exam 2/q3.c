ef88bcb1a5bd4eff0b88d85b0f79951b75d2c936
Uniquesoul18 <soumyatae1831@gmail.com>
2024-09-06 10:23:49 +0530

    q3.c

--- Exam 2/q3.c
+++ Exam 2/q3.c
@@ -1,19 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+
 //WAP to input any charecter and check wheter its alphabate,digit or special charecter//
 
 int main()
 {
-	
-	char choice;
-	printf("Enter your choice :");
-	scanf("%c",&choice);
-	
-	if(choice<='a'<='z')
-	{
-		printf("It is a alphabate");
-	}
-	
-	
-	
-	return 0;
+    char choice;
+
+    /* Input character from user */
+    printf("Enter any character: ");
+    scanf("%c", &choice);
+
+
+    /* Alphabet check */
+    if((choice >= 'a' && choice <= 'z') || (choice >= 'A' && choice <= 'Z'))
+    {
+        printf("'%c' is alphabet.", choice);
+    }
+    else if(choice >= '0' && choice <= '9')
+    {
+        printf("'%c' is digit.", choice);
+    }
+    else 
+    {
+        printf("'%c' is special character.", choice);
+    }
+
+    return 0;
 }
